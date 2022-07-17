834102e08177199ee040c637aaab8cf5e1f6bf99
Alireza <ali1985reza@gmail.com>
2022-07-17 21:33:14 +1000

    data was stored in the localstorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,11 +1,17 @@
-import React,{createContext, useReducer} from 'react';
+import React,{createContext, useEffect, useReducer} from 'react';
 import { bookReducer } from '../reducers/BookReducer';
 
 
 export const Bookcontext = createContext();
 
 const BookContextProvider = (props) => {
-const [books, dispatch] = useReducer(bookReducer, []);
+const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const data = localStorage.getItem('books');
+    return data ? JSON.parse(data) : []
+})
+useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books))
+}, [books])
 
     return ( 
         <Bookcontext.Provider value={{ books, dispatch }}>
